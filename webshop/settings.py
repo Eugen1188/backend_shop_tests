ef516ebc1e8ba88a22b8e4c082ffc77226ff8eda
Eugen1188 <eugen.ferchow@gmx.de>
2025-05-30 01:55:39 +0200

    allowed-hostet_change

--- webshop/settings.py
+++ webshop/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['https://backend-shop-tests-2.onrender.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['backend-shop-tests-2.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
