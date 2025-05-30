93d5c16f1c4462f69096751f22b6c9d20ac6a7c1
Eugen1188 <eugen.ferchow@gmx.de>
2025-05-30 22:50:33 +0200

    add start.sh

--- webshop/settings.py
+++ webshop/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -84,10 +85,7 @@ WSGI_APPLICATION = 'webshop.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+      'default': dj_database_url.config(conn_max_age=600)
 }
 
 
