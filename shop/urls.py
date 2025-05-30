48142441ec5d6bec23450b2727c3a6990ac2c571
Eugen1188 <eugen.ferchow@gmx.de>
2025-05-30 18:09:27 +0200

    urls: add categories url

--- shop/urls.py
+++ shop/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import ProductListCreateAPIView
+from .views import ProductListCreateAPIView, ProductDetailAPIView
 
 urlpatterns = [
     path('products/', ProductListCreateAPIView.as_view(), name='product-list'),
+    path('categories/', ProductDetailAPIView.as_view(), name='category-list'),
 ]
