f0f162801b247a34ca15c606e0d35c4ec0797a12
Eugen1188 <eugen.ferchow@gmx.de>
2025-05-30 23:12:51 +0200

    change gunicorn path

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
-gunicorn backend_shop.wsgi:application --bind 0.0.0.0:8000
+gunicorn webshop.wsgi:application --bind 0.0.0.0:8000
