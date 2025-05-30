0e9fd0f7ec41a4ca7e144848fb56d51fe41744bb
Eugen1188 <eugen.ferchow@gmx.de>
2025-05-30 02:15:21 +0200

    change Product model

--- shop/models.py
+++ shop/models.py
@@ -8,5 +8,9 @@ class Category(models.Model):
 
 class Product(models.Model):
     category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE, null=True, blank=True)
+    name = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
 
-
+    def __str__(self):
+        return self.name
