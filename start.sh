b8441094d6d3e05998f762511a225d573224e96f
Eugen1188 <eugen.ferchow@gmx.de>
2025-05-30 22:55:13 +0200

    add start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
-gunicorn backend_shop.wsgi:application --bind 0.0.0.0:$PORT
+gunicorn backend_shop.wsgi:application --bind 0.0.0.0:8000
