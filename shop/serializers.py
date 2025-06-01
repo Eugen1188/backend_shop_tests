30014cab6a9452252332daffa7fd8d8d8e96f422
Eugen1188 <eugen.ferchow@gmx.de>
2025-06-02 00:03:40 +0200

    fix: serializerbug

--- shop/serializers.py
+++ shop/serializers.py
@@ -1,27 +1,21 @@
 from rest_framework import serializers
 from .models import Product, Category
 
-class CategorySerializer(serializers.ModelSerializer):
-    products = serializers.SerializerMethodField()
-
-    class Meta:
-        model = Category
-        fields = ['id', 'name', 'products']
-
-    def get_products(self, obj):
-        products = obj.products.all()
-        return ProductSerializer(products, many=True).data
-
-
 class ProductSerializer(serializers.ModelSerializer):
-    category = CategorySerializer(read_only=True)
+    category = serializers.CharField(source='category.name', read_only=True)
     category_id = serializers.PrimaryKeyRelatedField(queryset=Category.objects.all(), source='category', write_only=True)
-
     class Meta:
         model = Product
         fields = ['id', 'name', 'description', 'price', 'category', 'category_id']
 
-    def get_category(self, obj):
-        return {"id": obj.category.id, "name": obj.category.name} if obj.category else None
+
+class CategorySerializer(serializers.ModelSerializer):
+    products = ProductSerializer(many=True, read_only=True)
+    class Meta:
+        model = Category
+        fields = ['id', 'name', 'products']
+
+
+
 
 
