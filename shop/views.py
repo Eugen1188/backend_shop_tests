546c4be41c200619df63a5c01e8bfbad8d8cde0e
Eugen1188 <eugen.ferchow@gmx.de>
2025-06-02 02:00:34 +0200

    test

--- shop/views.py
+++ shop/views.py
@@ -5,17 +5,17 @@ from .models import Category, Product
 from .serializers import CategorySerializer, ProductSerializer
 from rest_framework.generics import ListAPIView
 
-class ProductListCreateAPIView(generics.ListCreateAPIView):
-    queryset = Product.objects.all()
-    serializer_class = ProductSerializer
-    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
-    filterset_fields = ['category']
-    search_fields = ['name', 'description']
-    ordering_fields = ['price', 'name']
-
-# class ProductDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
+# class ProductListCreateAPIView(generics.ListCreateAPIView):
 #     queryset = Product.objects.all()
 #     serializer_class = ProductSerializer
+#     filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+#     filterset_fields = ['category']
+#     search_fields = ['name', 'description']
+#     ordering_fields = ['price', 'name']
+
+class ProductDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
 
 class CategoryListAPIView(ListAPIView):
     queryset = Category.objects.all()
