a3d819bb329af3d81cc8213fd616dc79dcb76dcc
Eugen1188 <eugen.ferchow@gmx.de>
2025-06-05 00:03:46 +0200

    test

--- shop/urls.py
+++ shop/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
 from .views import CategoryListAPIView, ProductListViewset
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register(r'products', ProductListViewset, basename='product')
 
 urlpatterns = [
-    path('products/', ProductListViewset.as_view(), name='product-list'),
+    # path('products/', ProductListViewset.as_view(), name='product-list'),
     path('categories/', CategoryListAPIView.as_view(), name='category-list'),
+    router.urls,
 ]
